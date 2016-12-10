7b80fe189b3a0c2675c70dc8fc81f88e4f344b32
TAKEKOSHI Akishige <akishige@takekoshi.net>
2016-12-10 22:36:56 +0900

    OutputEscaperをかける

---
+++ application/core/MY_Loader.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/BaseEscaper.php');
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/ArrayDecorator.php');
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/Escaper.php');
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/ObjectDecorator.php');
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/IteratorDecorator.php');
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/SafeDecoratorInterface.php');
+require_once(APPPATH . 'third_party/output_escaper/src/Symfony/Component/OutputEscaper/SafeDecorator.php');
+
+
+class MY_Loader extends CI_Loader
+{
+/*
+    public function view($view, $vars = array(), $return = FALSE)
+    {
+        return parent::view(
+            $view,
+            array_map(function($v){ return Symfony\Component\OutputEscaper\Escaper::escape('htmlspecialchars', $v); }, $vars),
+            $return
+        );
+    }
+*/
+}
